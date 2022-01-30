c5e877b22b857c4aafa7af0fbd37e0ce2b09c094
Beauclair Ngnintedem <beauclair.dongmo.ngnintedem@cofinpro.de>
2022-01-30 19:50:09 +0100

    CHG: delete unused constant

--- Cinema Room REST Service/task/src/cinema/util/Message.java
+++ Cinema Room REST Service/task/src/cinema/util/Message.java
@@ -2,7 +2,6 @@ package cinema.util;
 
 public class Message {
 
-    public static final String EMPTY_ERROR = "";
     public static final String PURCHASE_ERROR = "The ticket has been already purchased!";
     public static final String OUT_OF_BOUND_ERROR = "The number of a row or a column is out of bounds!";
     public static final String WRONG_TOKEN = "Wrong token!";
